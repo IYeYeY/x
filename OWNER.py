5fc062dee0855361a8ef8a69ecf879621c3bb3ac
𖥻 𝗧𝗵𝗲 𓏺 𝗖𝗟𝗘𝗩𝗘𝗥 .🇵🇸 <abdojackxx@gmail.com>
2025-08-10 17:34:49 +0300

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,22 @@
+# متطلبات التنصيب #
+
+#يوزر المطور
+OWNER = ["CL_3Q"] 
+OWNER__ID = 1471433717
+OWNER_DEVELOPER = 1471433717
+OWNER_NAME = "𓏺 َ𝗬𝗼𝘂𝘀𝗲𝗳"
+#اسم التي سيظهر على صورة
+infophoto = "Alsayed Playing"
+DATABASE = "mongodb+srv://huSeen96:Huseenslah96@cluster0.ld2v7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" 
+#قناة سورس
+CHANNEL = "https://t.me/x_v_l"
+#كروب سورس
+GROUP = "https://t.me/PRTRZA"
+#فيديو سورس اذا ماعندك خليه
+VIDEO = "https://t.me/MusicxXxYousef/91"
+#صورة سورس اذا ماعندك خليها
+PHOTO = "https://t.me/MusicxXxYousef/90"
+#ساوي كروب وضيف عليه مصنع ورفعو اشراف وحط يوزر الكروب هنا
+LOGS = "xjjfjfhh"
+ 
+ 
