cd5baddc6199921664acac51a0ab54e148943eaf
𖥻 𝗧𝗵𝗲 𓏺 𝗖𝗟𝗘𝗩𝗘𝗥 .🇵🇸 <abdojackxx@gmail.com>
2025-10-11 09:02:22 +0300

    Update play.py

--- CLeVeRMusic/utils/inline/play.py
+++ CLeVeRMusic/utils/inline/play.py
@@ -1,7 +1,5 @@
 import math
-
 from pyrogram.types import InlineKeyboardButton
-
 from CLeVeRMusic.utils.formatters import time_to_seconds
 
 
@@ -23,50 +21,25 @@ def track_markup(_, videoid, user_id, channel, fplay):
                 callback_data=f"forceclose {videoid}|{user_id}",
             )
         ],
-    ]
-    return buttons
-
-
-def stream_markup_timer(_, chat_id, played, dur):
-    played_sec = time_to_seconds(played)
-    duration_sec = time_to_seconds(dur)
-    percentage = (played_sec / duration_sec) * 100
-    umm = math.floor(percentage)
-    if 0 < umm <= 10:
-        bar = "⦷─────────"
-    elif 10 < umm < 20:
-        bar = "─⦷────────"
-    elif 20 <= umm < 30:
-        bar = "──⦷───────"
-    elif 30 <= umm < 40:
-        bar = "───⦷──────"
-    elif 40 <= umm < 50:
-        bar = "────⦷─────"
-    elif 50 <= umm < 60:
-        bar = "─────⦷────"
-    elif 60 <= umm < 70:
-        bar = "──────⦷───"
-    elif 70 <= umm < 80:
-        bar = "───────⦷──"
-    elif 80 <= umm < 95:
-        bar = "────────⦷─"
-    else:
-        bar = "─────────⦷"
-    buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),        
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{channel}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{channel}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{channel}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{channel}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{channel}"),
         ],
         [
             InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
+                text=f"اوهام .",
+                url="https://t.me/xG_Ln",
             )
         ],
-        [InlineKeyboardButton(text=f"اوهام .", url=f"https://t.me/xG_Ln")],
+        [
+            InlineKeyboardButton(
+                text="اضف البوت الي مجموعتك او قناتك⋡",
+                url=f"https://t.me/{_['bot_username']}?startgroup=true",
+            )
+        ],
     ]
     return buttons
 
@@ -78,9 +51,17 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),           
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [InlineKeyboardButton(text=f"اوهام .", url=f"https://t.me/xG_Ln")],
+        [
+            InlineKeyboardButton(text="اوهام .", url="https://t.me/xG_Ln"),
+        ],
+        [
+            InlineKeyboardButton(
+                text="اضف البوت الي مجموعتك او قناتك⋡",
+                url=f"https://t.me/{_['bot_username']}?startgroup=true",
+            )
+        ],
     ]
     return buttons
 
